c80f1010af11a73309e012c0bfaf21bae2087a41
Anmol Rai <anmolrai853@gmail.com>
2021-11-02 15:29:29 +0000

    Added Welcome screen

--- Fundamental of programming/Console games/Program.cs
+++ Fundamental of programming/Console games/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using System.Linq;
+using System.Threading;
 namespace Console_games
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"                (             )     *                      )   
+ (  (           )\ )   (   ( /(   (  `           *   )  ( /(   
+ )\))(   ' (   (()/(   )\  )\())  )\))(   (    ` )  /(  )\())  
+((_)()\ )  )\   /(_))(((_)((_)\  ((_)()\  )\    ( )(_))((_)\   
+_(())\_)()((_) (_))  )\___  ((_) (_()((_)((_)  (_(_())   ((_)  
+\ \((_)/ /| __|| |  ((/ __|/ _ \ |  \/  || __| |_   _|  / _ \  
+ \ \/\/ / | _| | |__ | (__| (_) || |\/| || _|    | |   | (_) | 
+  \_/\_/  |___||____| \___|\___/ |_|  |_||___|   |_|    \___/  
+                                                               "); //says welcome
+            Thread.Sleep(1400);
+        Console.WriteLine(@"╔╦╗╦╔═╗  ╔╦╗╔═╗╔═╗  ╔╦╗╔═╗╔═╗
+ ║ ║║     ║ ╠═╣║     ║ ║ ║║╣ 
+ ╩ ╩╚═╝   ╩ ╩ ╩╚═╝   ╩ ╚═╝╚═╝"); //says tictactoe
+
+        string [,]  array = new string[,]
+        {
+            {"blank", "blank" },
         }
     }
 }
